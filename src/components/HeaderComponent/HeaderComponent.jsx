899a90092ce46cb0ecdf4125ce787684b991233f
minhln.tt <minhln.tt@mcredit.com.vn>
2024-03-10 11:07:36 +0700

    UI Header

--- src/components/HeaderComponent/HeaderComponent.jsx
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -1,16 +1,49 @@
-import { Col, Row } from 'antd'
+import { Col } from 'antd'
 import React from 'react'
+import { WapperHeaderAccount, WapperTextHeader, WapperTextHeaderSmall, WrapperHeader } from './style'
+import Search from 'antd/es/transfer/search'
+import {
+  UserOutlined,
+  CaretDownOutlined,
+  ShoppingCartOutlined
+
+} from '@ant-design/icons';
 
 const HeaderComponent = () => {
   return (
     <div>
-      <Row>
-      <Col span={6}>col-8</Col>
-      <Col span={12}>col-8</Col>
-      <Col span={6}>
-        col-8
-      </Col>
-    </Row>
+      <WrapperHeader gutter={16}>
+        <Col span={6}>
+          <WapperTextHeader>ECOMMERCE WEB</WapperTextHeader>
+        </Col>
+
+        <Col span={12}>
+          <Search
+            placeholder="input search text"
+            allowClear
+            enterButton="Search"
+            size="large"
+          // onSearch={onSearch}
+          />
+        </Col>
+
+        <Col span={6} style={{display: 'flex', gap: '20px'}}>
+          <WapperHeaderAccount>
+            <UserOutlined style={{ fontSize: '36px' }} />
+            <div>
+              <WapperTextHeaderSmall>Đăng nhập/Đăng ký</WapperTextHeaderSmall>
+              <div>
+                <WapperTextHeaderSmall>Tài khoản</WapperTextHeaderSmall>
+                <CaretDownOutlined />
+              </div>
+            </div>
+          </WapperHeaderAccount>
+          <div>
+            <ShoppingCartOutlined style={{ fontSize: '36px', color: '#fff' }} />
+            <WapperTextHeaderSmall>Giỏ hàng</WapperTextHeaderSmall>
+          </div>
+        </Col>
+      </WrapperHeader>
     </div>
   )
 }
---
+++ src/components/HeaderComponent/style.js
@@ -0,0 +1,24 @@
+import { Row } from "antd";
+import styled from "styled-components";
+
+export const WrapperHeader = styled(Row) `
+    padding: 10px 120px;
+    background-color: rgb(26, 148, 255);
+    align-items: center;
+`
+export const WapperTextHeader = styled.span`
+    font-size: 18px;
+    color: #fff;
+    font-weight: bold,
+    text-align: left
+`
+export const WapperHeaderAccount = styled.div`
+    display: flex;
+    align-items: center;
+    color: #fff;
+    gap: 10px;
+`
+export const WapperTextHeaderSmall = styled.span`
+    font-size: 12px;
+    color: #fff
+`
