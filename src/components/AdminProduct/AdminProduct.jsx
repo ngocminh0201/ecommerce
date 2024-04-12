df8086e27f33ad3680aaadb8372fe8689947be83
thelij2000 <80305593+thelij2000@users.noreply.github.com>
2024-04-13 01:20:32 +0700

    Update sản phẩm

--- src/components/AdminProduct/AdminProduct.jsx
+++ src/components/AdminProduct/AdminProduct.jsx
@@ -11,10 +11,16 @@ import { useMutationHooks } from "../../hooks/useMutationHook";
 import Loading from "../LoadingComponent/Loading";
 import * as message from "../../components/Message/Message";
 import { useQuery } from "@tanstack/react-query";
+import DrawerComponent from "../DrawerComponent/DrawerComponent";
+import { useSelector } from "react-redux";
 
 const AdminProduct = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [stateProduct, setStateProduct] = useState({
+  const [rowSelected, setRowSelected] = useState("");
+  const user = useSelector((state) => state?.user);
+  const [isPendingUpdate, setIsPendingUpdate] = useState(false);
+  const [isOpenDrawer, setIsOpenDrawer] = useState(false);
+  const inittial = () => ({
     name: "",
     price: "",
     description: "",
@@ -23,19 +29,15 @@ const AdminProduct = () => {
     type: "",
     countInStock: "",
   });
+  const [stateProduct, setStateProduct] = useState(inittial());
+
+  const [stateProductDetails, setStateProductDetails] = useState(inittial());
 
   const [form] = Form.useForm();
 
   const mutation = useMutationHooks((data) => {
-    const {
-      name,
-      price,
-      description,
-      rating,
-      image,
-      type,
-      countInStock: countInStock,
-    } = data;
+    const { name, price, description, rating, image, type, countInStock } =
+      data;
     const res = ProductService.createProduct({
       name,
       price,
@@ -48,12 +50,60 @@ const AdminProduct = () => {
     return res;
   });
 
+  const mutationUpdate = useMutationHooks((data) => {
+    const { id, token, ...rests } = data;
+    const res = ProductService.updateProduct(id, token, { ...rests });
+    return res;
+  });
+
   const getAllProducts = async () => {
     const res = await ProductService.getAllProduct();
     return res;
   };
 
+  const fetchGetDetailsProduct = async (rowSelected) => {
+    const res = await ProductService.getDetailsProduct(rowSelected);
+    if (res?.data) {
+      setStateProductDetails({
+        name: res?.data?.name,
+        price: res?.data?.price,
+        description: res?.data?.description,
+        rating: res?.data?.rating,
+        image: res?.data?.image,
+        type: res?.data?.type,
+        countInStock: res?.data?.countInStock,
+        discount: res?.data?.discount,
+      });
+    }
+    setIsPendingUpdate(false);
+  };
+
+  useEffect(() => {
+    form.setFieldsValue(stateProductDetails);
+  }, [form, stateProductDetails]);
+
+  useEffect(() => {
+    if (rowSelected) {
+      // setIsPendingUpdate(true)
+      fetchGetDetailsProduct(rowSelected);
+    }
+  }, [rowSelected]);
+
+  const handleDetailsProduct = () => {
+    if (rowSelected) {
+      setIsPendingUpdate(true);
+      fetchGetDetailsProduct();
+    }
+    setIsOpenDrawer(true);
+  };
+
   const { data, isPending, isSuccess, isError } = mutation;
+  const {
+    data: dataUpdated,
+    isPending: isPendingUpdated,
+    isSuccess: isSuccessUpdated,
+    isError: isErrorUpdated,
+  } = mutationUpdate;
   const { isPending: isPendingProducts, data: products } = useQuery({
     queryKey: ["product"],
     queryFn: getAllProducts,
@@ -66,6 +116,7 @@ const AdminProduct = () => {
         />
         <EditOutlined
           style={{ color: "orange", fontSize: "30px", cursor: "pointer" }}
+          onClick={handleDetailsProduct}
         />
       </div>
     );
@@ -109,6 +160,29 @@ const AdminProduct = () => {
     }
   }, [isSuccess]);
 
+  const handleCloseDrawer = () => {
+    setIsOpenDrawer(false);
+    setStateProductDetails({
+      name: "",
+      price: "",
+      description: "",
+      rating: "",
+      image: "",
+      type: "",
+      countInStock: "",
+    });
+    form.resetFields();
+  };
+
+  useEffect(() => {
+    if (isSuccessUpdated && dataUpdated?.status === "OK") {
+      message.success();
+      handleCloseDrawer();
+    } else if (isErrorUpdated) {
+      message.error();
+    }
+  }, [isSuccessUpdated]);
+
   const handleOnchangeAvatar = async ({ fileList }) => {
     const file = fileList[0];
     if (!file.url && !file.preview) {
@@ -118,6 +192,26 @@ const AdminProduct = () => {
       ...stateProduct,
       image: file.preview,
     });
+  };
+  const handleOnchangeAvatarDetails = async ({ fileList }) => {
+    const file = fileList[0];
+    if (!file.url && !file.preview) {
+      file.preview = await getBase64(file.originFileObj);
+    }
+    setStateProductDetails({
+      ...stateProductDetails,
+      image: file.preview,
+    });
+  };
+  const onUpdateProduct = () => {
+    mutationUpdate.mutate(
+      { id: rowSelected, token: user?.access_token, ...stateProductDetails }
+      //   , {
+      //   onSettled: () => {
+      //     queryProduct.refetch()
+      //   }
+      // }
+    );
   };
   const handleCancel = () => {
     setIsModalOpen(false);
@@ -135,6 +229,13 @@ const AdminProduct = () => {
   const handleOnchange = (e) => {
     setStateProduct({
       ...stateProduct,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleOnchangeDetails = (e) => {
+    setStateProductDetails({
+      ...stateProductDetails,
       [e.target.name]: e.target.value,
     });
   };
@@ -164,6 +265,13 @@ const AdminProduct = () => {
           columns={columns}
           isPending={isPendingProducts}
           data={dataTable}
+          onRow={(record, rowIndex) => {
+            return {
+              onClick: (event) => {
+                setRowSelected(record._id);
+              },
+            };
+          }}
         />
       </div>
       <Modal
@@ -293,6 +401,136 @@ const AdminProduct = () => {
           </Form>
         </Loading>
       </Modal>
+      <DrawerComponent
+        title="Chi tiết sản phẩm"
+        isOpen={isOpenDrawer}
+        onClose={() => setIsOpenDrawer(false)}
+        width="90%"
+      >
+        <Loading isPending={isPendingUpdate}>
+          <Form
+            name="basic"
+            labelCol={{ span: 2 }}
+            wrapperCol={{ span: 22 }}
+            onFinish={onUpdateProduct}
+            autoComplete="on"
+            form={form}
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true, message: "Please input your name!" }]}
+            >
+              <InputComponent
+                value={stateProductDetails.name}
+                onChange={handleOnchangeDetails}
+                name="name"
+              />
+            </Form.Item>
+
+            <Form.Item
+              label="Type"
+              name="type"
+              rules={[{ required: true, message: "Please input your type!" }]}
+            >
+              <InputComponent
+                value={stateProductDetails.type}
+                onChange={handleOnchangeDetails}
+                name="type"
+              />
+            </Form.Item>
+
+            <Form.Item
+              label="Count inStock"
+              name="countInStock"
+              rules={[
+                { required: true, message: "Please input your count inStock!" },
+              ]}
+            >
+              <InputComponent
+                value={stateProductDetails.countInStock}
+                onChange={handleOnchangeDetails}
+                name="countInStock"
+              />
+            </Form.Item>
+            <Form.Item
+              label="Price"
+              name="price"
+              rules={[
+                { required: true, message: "Please input your count price!" },
+              ]}
+            >
+              <InputComponent
+                value={stateProductDetails.price}
+                onChange={handleOnchangeDetails}
+                name="price"
+              />
+            </Form.Item>
+            <Form.Item
+              label="Description"
+              name="description"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your count description!",
+                },
+              ]}
+            >
+              <InputComponent
+                value={stateProductDetails.description}
+                onChange={handleOnchangeDetails}
+                name="description"
+              />
+            </Form.Item>
+            <Form.Item
+              label="Rating"
+              name="rating"
+              rules={[
+                { required: true, message: "Please input your count rating!" },
+              ]}
+            >
+              <InputComponent
+                value={stateProductDetails.rating}
+                onChange={handleOnchangeDetails}
+                name="rating"
+              />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 20, span: 16 }}>
+              <Button type="primary" htmlType="submit">
+                Apply
+              </Button>
+            </Form.Item>
+            <Form.Item
+              label="Image"
+              name="image"
+              rules={[
+                { required: true, message: "Please input your count image!" },
+              ]}
+            >
+              <WrapperUploadFile
+                onChange={handleOnchangeAvatarDetails}
+                maxCount={1}
+              >
+                <Button>Select File</Button>
+                {stateProductDetails?.image && (
+                  <img
+                    src={stateProductDetails?.image}
+                    style={{
+                      height: "60px",
+                      width: "60px",
+                      borderRadius: "50%",
+                      objectFit: "cover",
+                      marginLeft: "10px",
+                    }}
+                    alt="avatar"
+                  />
+                )}
+              </WrapperUploadFile>
+            </Form.Item>
+          </Form>
+        </Loading>
+      </DrawerComponent>
     </div>
   );
 };
--- src/components/TableComponent/TableComponent.jsx
+++ src/components/TableComponent/TableComponent.jsx
@@ -59,6 +59,7 @@ const TableComponent = (props) => {
         }}
         columns={columns}
         dataSource={data}
+        {...props}
       />
     </Loading>
   );
